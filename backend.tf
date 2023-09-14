c31e9199ae0ab79ec30c522c678a13eaa4015c2d
Hendry Setiawan <hndry.st@gmail.com>
2023-09-14 17:00:31 +0700

    add comment

--- backend.tf
+++ backend.tf
@@ -6,6 +6,8 @@ terraform {
 }
 
 /*
-need backend.tfvars file with the bucket name
+terraform init -backend-config=backend.hcl
+need backend.hcl file with the bucket name and prefix
 bucket = "bucketname"
+prefix = "dirname/state"
 */
