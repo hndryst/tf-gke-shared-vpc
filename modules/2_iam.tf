b8839dd097e7764510e46724c731893009fc0a26
Hendry Setiawan <hndry.st@gmail.com>
2023-09-14 17:31:28 +0700

    modify iam assignment for SA

--- modules/2_iam.tf
+++ modules/2_iam.tf
@@ -1,10 +1,11 @@
-resource "google_service_account_iam_member" "iam" {
+resource "google_project_iam_member" "iam" {
+  project = var.project
   for_each = toset([
     #"roles/logging.logWriter",
     #"roles/logging.viewer",
     #"roles/monitoring.metricWriter",
     "roles/container.nodeServiceAccount"
   ])
-  role               = each.key
-  service_account_id = google_service_account.sa.name
+  role   = each.key
+  member = "serviceAccount:${google_service_account.sa.email}"
 }
