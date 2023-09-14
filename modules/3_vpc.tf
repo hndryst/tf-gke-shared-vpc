39aed75596f844770f67579be9f46025b1f79919
Hendry Setiawan <hndry.st@gmail.com>
2023-09-14 17:05:54 +0700

    update resource name

--- modules/3_vpc.tf
+++ modules/3_vpc.tf
@@ -34,7 +34,7 @@ resource "google_service_networking_connection" "psa-1-attach" {
   reserved_peering_ranges = [google_compute_global_address.psa-1.name]
 }
 resource "google_compute_network_peering_routes_config" "psa-1-route" {
-  peering = google_service_networking_connection.psa-1.name
+  peering = google_service_networking_connection.psa-1-attach.name
   network = google_compute_network.vpc.name
 
   import_custom_routes = true
